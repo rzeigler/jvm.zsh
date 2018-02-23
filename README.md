1d2b14288a7df3fe3bb4db94e86596d9ec05b903
Ryan Zeigler <ryan.zeigler@s5stratos.com>
2018-02-23 08:38:17 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# jvm.zsh
+
+A zsh plugin to quickly manage the version of java and scala on macos.
+Assumes that scala was installed with homebrew.
---
+++ jvm.zsh
@@ -0,0 +1,34 @@
+function use_java {
+    sys_java
+    export JAVA_VERSION="$1"
+    export JAVA_HOME=$(/usr/libexec/java_home -v ${JAVA_VERSION})
+    path=("$JAVA_HOME/bin" $path)
+}
+
+function default_java {
+    if [ ! -z ${JAVA_HOME+x} ]
+    then
+        path=(${(@)path:#"${JAVA_HOME}/bin"})
+        unset JAVA_HOME
+    fi
+}
+
+function use_scala {
+    export USE_SCALA_HOME="/usr/local/opt/scala@$1"
+    if [ ! -d "$USE_SCALA_HOME" ]
+    then
+        echo "${1} is not an installed scala version" 1>&2
+        exit 1
+    fi
+    default_scala
+    export SCALA_HOME="${USE_SCALA_HOME}"
+    path=("$SCALA_HOME/bin" $path)
+}
+
+function default_scala {
+    if [ ! -z ${SCALA_HOME+x} ]
+    then
+        path=(${(@)path:#"${SCALA_HOME}/bin"})
+        unset SCALA_HOME
+    fi
+}
